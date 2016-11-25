e4cbd49f23dcb3f3bcaac4435b3ce1e46cf2df0f
lsh <lsh@lsh-PC>
2016-11-25 15:04:21 +0900

    commit3

--- src/test/etc/HelloWorld.java
+++ src/test/etc/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("dddd");
 		System.out.println("commit1");
+		System.out.println("commit2");
+		System.out.println("commit3");
 		
 		//fsfsdfs
 	}
